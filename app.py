bddc7a639c40318a9adb5190ecc9a9fa5570dbc0
Ravi keerthi R <ravikeerthi2006@gmail.com>
2026-01-29 23:34:52 +0530

    Replace Tkinter with Gradio for HF Space

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-import tkinter as tk
-from tkinter import filedialog
-from PIL import Image, ImageTk
+import gradio as gr
 import tensorflow as tf
 import numpy as np
+from PIL import Image
 import json
 
-# Load saved model
-model = tf.keras.models.load_model("model.keras",compile=False)
+# Load model
+model = tf.keras.models.load_model("model.keras", compile=False)
 
 # Load class names
 with open("class_names.json", "r") as f:
@@ -14,46 +13,25 @@ with open("class_names.json", "r") as f:
 
 IMG_SIZE = (224, 224)
 
-def predict_image(img_path):
-    img = Image.open(img_path).convert("RGB")
-    img = img.resize(IMG_SIZE)
-    img_array = np.array(img) / 255.0
+def predict(image):
+    image = image.resize(IMG_SIZE)
+    img_array = np.array(image) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
 
-    predictions = model.predict(img_array)
-    idx = np.argmax(predictions[0])
-    confidence = predictions[0][idx]
+    preds = model.predict(img_array)
+    confidence = float(np.max(preds))
+    predicted_class = class_names[np.argmax(preds)]
 
-    return class_names[idx], confidence
+    return {
+        predicted_class: confidence
+    }
 
-def upload_image():
-    file_path = filedialog.askopenfilename()
-    if not file_path:
-        return
+demo = gr.Interface(
+    fn=predict,
+    inputs=gr.Image(type="pil", label="Upload Leaf Image"),
+    outputs=gr.Label(num_top_classes=5, label="Prediction"),
+    title="🌿 Plant Disease Classification",
+    description="Upload a plant leaf image to detect disease using deep learning"
+)
 
-    img = Image.open(file_path).resize((300,300))
-    img_tk = ImageTk.PhotoImage(img)
-    image_label.config(image=img_tk)
-    image_label.image = img_tk
-
-    label, conf = predict_image(file_path)
-    result_label.config(text=f"Disease: {label}\nConfidence: {conf*100:.2f}%")
-
-# UI
-root = tk.Tk()
-root.title("Plant Disease Classifier")
-root.geometry("450x550")
-
-title = tk.Label(root, text="Plant Disease Detector", font=("Arial", 16))
-title.pack(pady=10)
-
-image_label = tk.Label(root)
-image_label.pack(pady=10)
-
-btn = tk.Button(root, text="Upload Image", command=upload_image)
-btn.pack(pady=10)
-
-result_label = tk.Label(root, text="", font=("Arial", 14))
-result_label.pack(pady=10)
-
-root.mainloop()
+demo.launch()
